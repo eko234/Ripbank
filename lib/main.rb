1b3a25b1dbe49694678e4f032150843cd38e983c
Luis Alfonso Buelvas Betancourt <38670883+eko234@users.noreply.github.com>
2021-11-17 16:01:30 -0600

    Update main.rb

--- lib/main.rb
+++ lib/main.rb
@@ -1,22 +1,38 @@
 require 'sinatra'
-require 'yaml/store'
 require 'json'
+require 'pstore'
 
-db_path = './db.yaml'
+db_path = './db'
 
 # API
 before '/api/*' do
-  @store = YAML::Store.new db_path
+  request.headers['Authorization'] == 'Bearer passpass' or halt 401  
+  response.headers['Content-Type'] = 'application/json'
+  @store = PStore.new db_path
 end
 
 get '/api/clients' do
   result = @store.transaction do
     @store['clients']
-  end
-  result
+  end  
+  result.to_json
 end
 
-put '/api/clients/:client_id' do
+before '/api/clients/:client_id' do
+  result = @store.transaction do
+    @store['clients'].key? params['client_id'] or error 'Invalid id'
+  end
+  result.to_json
+end
+
+get '/api/clients/:client_id' do
+  result = @store.transaction do
+     @store['clients'][params['client_id']]
+  end
+  result.to_json
+end
+
+patch '/api/clients/:client_id' do
   result = @store.transaction do
     request.body.rewind
     old = @store['clients'][params['client_id']]
@@ -24,26 +40,5 @@ put '/api/clients/:client_id' do
     @store['clients'][params['client_id']] = old.merge new
     @store['clients'][params['client_id']]
   end
-  [:ok, result].to_json
-end
-
-# PAGES
-get '/' do
-  "<h1> RIP </h1>"
-end
-
-before '/:client_id' do
-  @store = YAML::Store.new db_path
-  @store.transaction do
-    @store['clients'].key? params['client_id'] or error 'Invalid id'
-  end
-end
-
-get '/:client_id' do
-  result = @store.transaction do
-    @store['clients'][params['client_id']]
-  end
-  ip = result['ip']
-  port = result['port']
-  "<a href=\"http://#{ip}\" target=\"_blank\" > link </a>"
+  result.to_json
 end
